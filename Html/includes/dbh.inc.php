19bec38ca292b729b5bed98270d2665461b20751
ElShake <www.sanu95@hotmail.it>
2020-08-27 15:36:00 +0200

    First commit

---
+++ Html/includes/dbh.inc.php
@@ -0,0 +1,8 @@
+<?php
+try {
+  $dsn = "mysql:host=localhost;dbname=invodb;charset=utf8";
+  $PDOconn = new PDO($dsn, "sam", "Pollastri666");
+  $PDOconn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+  echo $e->getMessage();
+};
